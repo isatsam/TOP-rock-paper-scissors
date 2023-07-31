df153df50f45595edb6ccce9981bfea1a27b2ef6
tigeritest <59480088+tigeritest@users.noreply.github.com>
2023-08-01 00:24:09 +0300

    Get player's choice through a prompt(), for now

--- game.js
+++ game.js
@@ -5,3 +5,19 @@ function getComputerChoice (array) {
     var computerChoice = array[choiceNumber];
     return computerChoice;
 }
+
+// Gets player's choice via a prompt()
+function promptPlayerChoice (array) {
+    var playerInput = prompt("Rock, Paper, Scissors?");
+    playerInput = playerInput.toLowerCase();
+
+    // Prompt again if we got a string that's not in the array from the user
+    while (typeof playerInput == string && array.includes(playerInput) == false) {
+        let playerInput = prompt("Come on! Rock, Paper, Scissors?");
+    }
+
+    if (typeof playerInput == string) {
+        return playerInput;
+    }
+
+}
