e75b98e3de7925f4229e64854e9657210c5cf895
tigeritest <59480088+tigeritest@users.noreply.github.com>
2023-08-07 16:44:55 +0300

    Move to integers as choices and shorten game logic

--- scripts/game.js
+++ scripts/game.js
@@ -1,71 +1,23 @@
-function getComputerChoice () {
-    array = ["rock", "paper", "scissors"];
-    let choiceNumber = Math.floor(Math.random()*array.length);
-    let computerChoice = array[choiceNumber];
-    return computerChoice;
-}
-
-// Protection against console
-function setPlayerChoice (choice) {
-    array = ["rock", "paper", "scissors"]
-    while (!array.includes(choice)) {
-        console.log("Hey! Play by the rules!");
-    }
-
-    return choice;
-}
-
-function decideWinner (playerChoice, computerChoice) {
-    let winner;
-
-    if (playerChoice == computerChoice) {
-        winner = "tie";
-    }
-    if (playerChoice == "rock") {
-        if (computerChoice == "scissors") {
-            winner = "player";
-        }
-    } else if (playerChoice == "paper") {
-        if (computerChoice == "rock") {
-            winner = "player";
-        }
-    } else if (playerChoice == "scissors") {
-        if (computerChoice == "paper") {
-            winner = "player";
-        }
-    }
-
-    if (winner != "tie" && winner != "player") {
-        winner = "computer"
-    }
-
-    return winner;
-}
-
-function playRound () {
-    choicesArray = ["rock", "paper", "scissors"]
-    let playerChoice = getPlayerChoice(choicesArray);
-    let computerChoice = getComputerChoice(choicesArray);
-
-    let winner = decideWinner(playerChoice, computerChoice);
-    if (winner == "tie") {
-        console.log("You both drew " + playerChoice + ".\nIt's a tie!")
+function pickWinner(player, computer) {
+    if (computer == 3 || player == 3) {
+        return Math.min(player, computer);
     } else {
-        console.log("You drew " + playerChoice + " and computer drew "
-             + computerChoice);
+        return Math.max(player, computer);
+    }  
+}
 
-        // Announcing the winner
-        if (winner == "computer") {
-            console.log("Computer won! Sorry about that!");
-        } else {
-            console.log("You won! Congratulations!");
-        }
+function getPlayerChoice(choice) {
+    array = ["rock", "paper", "scissors"];
+    if (!array.includes(choice)) {
+        console.log("Hey! Play by the rules!");
+    } else {
+        return array.indexOf(choice);
     }
 }
 
-function game (rounds) {
-    choices = ["rock", "paper", "scissors"];
-    for (let i = 0; i <= rounds; i++) {
-        playRound(choices)
-    }
+function playRound (playerClick) {
+    const computerChoice = () => Math.floor(Math.random() * (4 - 1) + 1);
+    const playerChoice = getPlayerChoice(playerClick);
+    let winner = decideWinner(playerChoice, computerChoice);
 }
+
