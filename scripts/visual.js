5d0fd25d5f5bd9420f6c74e498a066f41f7bb593
tigeritest <59480088+tigeritest@users.noreply.github.com>
2023-08-07 17:42:09 +0300

    Add a func for updating game's title

--- scripts/visual.js
+++ scripts/visual.js
@@ -9,4 +9,17 @@ function addScore(side) {
     const score = document.getElementById(side + "Score");
     score.textContent = +(score.textContent) + 1;
 }
+
+function announceRound(result, gameEnd) {
+    const announcement = document.getElementById("announcement");
+    if (result == "tie") { announcement.textContent = "Tie!"; }
+    else { 
+        result = result.slice(0, 1).toUpperCase() + result.slice(1);
+        if (announcement.textContent.includes(result)) {
+            announcement.textContent = `${result} won this round again!` 
+        } else {
+        announcement.textContent = `${result} won this round!` 
+        }
+    }
+}
 }
