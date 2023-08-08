32767f5375db8f84bbab653ea2cede9c5329039a
tigeritest <59480088+tigeritest@users.noreply.github.com>
2023-08-08 17:38:08 +0300

    Return to a simpler pickWin logic using strings

--- scripts/game.js
+++ scripts/game.js
@@ -1,30 +1,59 @@
 let currentRoundCount = 0;
 
-function pickWinner(playerClick, opponent) {
+function pickWinner(playerClick, opponentChoice) {
     let array = ["rock", "paper", "scissors"];
-    let player = array.indexOf(playerClick);
-    if (opponent == player) { 
-        // Equals = tie
-        return "tie";
-    } else if (opponent == 3 || player == 3) {
-        // Biggest value = winner
-        if (opponent < player) { return "player"; };
-    } else { 
-        // Smallest value = winner
-        if (opponent > player) { return "player"; };
+    if (!array.includes(playerClick)) {
+        console.log("Play by the rules!");
+        return;
     }
-    return "opponent";
+    let winner;
+
+    if (playerClick == opponentChoice) {
+        winner = "tie";
+    }
+    if (playerClick == "rock") {
+        if (opponentChoice == "scissors") {
+            winner = "player";
+        }
+    } else if (playerClick == "paper") {
+        if (opponentChoice == "rock") {
+            winner = "player";
+        }
+    } else if (playerClick == "scissors") {
+        if (opponentChoice == "paper") {
+            winner = "player";
+        }
+    }
+
+    if (winner != "tie" && winner != "player") {
+        winner = "opponent"
+    }
+
+    return winner;
 }
 
 function getOpponentChoice () {
-    return Math.floor(Math.random() * (4 - 1) + 1);
+    let array = ["rock", "paper", "scissors"];
+    return array[Math.floor(Math.random() * (array.length - 1) + 1)];
 }
 
 function playRound(playerClick) {
-    let opponentChoice = getOpponentChoice()
+    let opponentChoice = getOpponentChoice();
     let winner = pickWinner(playerClick, opponentChoice);
     currentRoundCount = currentRoundCount + 1;
-    addScore(winner); // This is the only "visual" function that we call inside game lo
+    addScore(winner); // This is the only "visual" function that we call inside game logic
+    console.log(winner);
+    console.log("Player's choice: " + playerClick)
+    console.log("Opponent's choice: " + opponentChoice)
+
+    toggleRoundResults(playerClick, opponentChoice);
+    toggleAllPlayer();
+    setTimeout(function() {
+        toggleAllPlayer();
+        toggleRoundResults(playerClick, opponentChoice);
+    }, 500);
+
+
     if (currentRoundCount >= 5) {
         endGame(winner);
     } else {
